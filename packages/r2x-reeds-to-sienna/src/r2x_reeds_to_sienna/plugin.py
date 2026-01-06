1906455515120934381530220630e37985fd21af
Marck Llerena V. <140716266+mcllerena@users.noreply.github.com>
2026-01-06 11:54:25 -0700

    feat: include r2s translation plugin configuration (#226)

---
+++ packages/r2x-reeds-to-sienna/src/r2x_reeds_to_sienna/plugin.py
@@ -0,0 +1,15 @@
+"""Entry point for plugin system."""
+
+from r2x_core import PluginManifest, PluginSpec
+
+from .translation import ReedsToSiennaTranslation
+
+manifest = PluginManifest(package="r2x-reeds-to-sienna")
+
+manifest.add(
+    PluginSpec.translation(
+        name="r2x-reeds-to-sienna.translation",
+        entry=ReedsToSiennaTranslation,
+        description="Translate ReEDS system to Sienna system."
+    )
+)
